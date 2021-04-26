e07ae4cec98b1e5deff397e29c79e8b630a632bb
Manoj-Paramsetti <paramsetti.manoj@gmail.com>
2021-04-26 10:22:04 +0530

    changed normal messages into embed messages

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import discord, os, requests, json
 from dotenv import load_dotenv
 
-pages = ["Page 1 of 4\nAfghanistan, Albania, Algeria, Andorra, Angola, Antigua and Barbuda, Argentina, Armenia, Australia, Austria, Azerbaijan, Bahamas, Bahrain, Bangladesh, Barbados, Belarus, Belgium, Belize, Benin, Bhutan, Bolivia, Bosnia and Herzegovina, Botswana, Brazil, Brunei, Bulgaria, Burkina Faso, Burma, Burundi, Cabo Verde, Cambodia, Cameroon, Canada, Central African Republic, Chad, Chile, China, Colombia, Comoros, Congo (Brazzaville), Congo (Kinshasa), Costa Rica, Cote d'Ivoire, Croatia, Cuba, Cyprus, Czechia",
-	"Page 2 of 4\nDenmark, Diamond Princess, Djibouti, Dominica, Dominican Republic, Ecuador, Egypt, El Salvador, Equatorial Guinea, Eritrea, Estonia, Eswatini, Ethiopia, Fiji, Finland, France, Gabon, Gambia, Georgia, Germany, Ghana, Greece, Grenada, Guatemala, Guinea, Guinea-Bissau, Guyana, Haiti, Holy See, Honduras, Hungary, Iceland, India, Indonesia, Iran, Iraq, Ireland, Israel, Italy, Jamaica, Japan, Jordan, Kazakhstan, Kenya, Korea, South, Kosovo, Kuwait, Kyrgyzstan, Laos, Latvia, Lebanon, Lesotho",
-	"Page 3 of 4\nLiberia, Libya, Liechtenstein, Lithuania, Luxembourg, MS Zaandam, Madagascar, Malawi, Malaysia, Maldives, Mali, Malta, Marshall Islands, Mauritania, Mauritius, Mexico, Micronesia, Moldova, Monaco, Mongolia, Montenegro, Morocco, Mozambique, Namibia, Nepal, Netherlands, New Zealand, Nicaragua, Niger, Nigeria, North Macedonia, Norway, Oman, Pakistan, Panama, Papua New Guinea, Paraguay, Peru, Philippines, Poland, Portugal, Qatar, Romania, Russia, Rwanda, Saint Kitts and Nevis, Saint Lucia",
-	"Page 4 of 4\nSaint Vincent and the Grenadines, Samoa, San Marino, Sao Tome and Principe, Saudi Arabia, Senegal, Serbia, Seychelles, Sierra Leone, Singapore, Slovakia, Slovenia, Solomon Islands, Somalia, South Africa, South Sudan, Spain, Sri Lanka, Sudan, Suriname, Sweden, Switzerland, Syria, Taiwan*, Tajikistan, Tanzania, Thailand, Timor-Leste, Togo, Trinidad and Tobago, Tunisia, Turkey, US, Uganda, Ukraine, United Arab Emirates, United Kingdom, Uruguay, Uzbekistan, Vanuatu, Venezuela, Vietnam, West Bank and Gaza, Yemen, Zambia, Zimbabwe"]
+pages = ["Afghanistan, Albania, Algeria, Andorra, Angola, Antigua and Barbuda, Argentina, Armenia, Australia, Austria, Azerbaijan, Bahamas, Bahrain, Bangladesh, Barbados, Belarus, Belgium, Belize, Benin, Bhutan, Bolivia, Bosnia and Herzegovina, Botswana, Brazil, Brunei, Bulgaria, Burkina Faso, Burma, Burundi, Cabo Verde, Cambodia, Cameroon, Canada, Central African Republic, Chad, Chile, China, Colombia, Comoros, Congo (Brazzaville), Congo (Kinshasa), Costa Rica, Cote d'Ivoire, Croatia, Cuba, Cyprus, Czechia",
+	"Denmark, Diamond Princess, Djibouti, Dominica, Dominican Republic, Ecuador, Egypt, El Salvador, Equatorial Guinea, Eritrea, Estonia, Eswatini, Ethiopia, Fiji, Finland, France, Gabon, Gambia, Georgia, Germany, Ghana, Greece, Grenada, Guatemala, Guinea, Guinea-Bissau, Guyana, Haiti, Holy See, Honduras, Hungary, Iceland, India, Indonesia, Iran, Iraq, Ireland, Israel, Italy, Jamaica, Japan, Jordan, Kazakhstan, Kenya, Korea, South, Kosovo, Kuwait, Kyrgyzstan, Laos, Latvia, Lebanon, Lesotho",
+	"Liberia, Libya, Liechtenstein, Lithuania, Luxembourg, MS Zaandam, Madagascar, Malawi, Malaysia, Maldives, Mali, Malta, Marshall Islands, Mauritania, Mauritius, Mexico, Micronesia, Moldova, Monaco, Mongolia, Montenegro, Morocco, Mozambique, Namibia, Nepal, Netherlands, New Zealand, Nicaragua, Niger, Nigeria, North Macedonia, Norway, Oman, Pakistan, Panama, Papua New Guinea, Paraguay, Peru, Philippines, Poland, Portugal, Qatar, Romania, Russia, Rwanda, Saint Kitts and Nevis, Saint Lucia",
+	"Saint Vincent and the Grenadines, Samoa, San Marino, Sao Tome and Principe, Saudi Arabia, Senegal, Serbia, Seychelles, Sierra Leone, Singapore, Slovakia, Slovenia, Solomon Islands, Somalia, South Africa, South Sudan, Spain, Sri Lanka, Sudan, Suriname, Sweden, Switzerland, Syria, Taiwan*, Tajikistan, Tanzania, Thailand, Timor-Leste, Togo, Trinidad and Tobago, Tunisia, Turkey, US, Uganda, Ukraine, United Arab Emirates, United Kingdom, Uruguay, Uzbekistan, Vanuatu, Venezuela, Vietnam, West Bank and Gaza, Yemen, Zambia, Zimbabwe"]
 
 #importing dotenv file
 load_dotenv()
@@ -39,12 +39,15 @@ async def on_message(message):
 	
 	#checks when someone says help
 	if message.content.startswith("!help"):
-		await message.channel.send('''**use these following commands:**
-`!hello` or `!corona help` - Says hello and shows how to open help
-`!corona` - To get global corona virus status
-`!corona countryname` - To get corona virus status in particular country
-`!corona list` - To get list 1
-`!corona list n` - To get list n. Here n is page number(1-4)''')
+		embed=discord.Embed(title="Help for Corona Bot", color=discord.Color.blue())
+		embed.add_field(name="!corona help", value="Says hello and shows how to open help", inline=True)
+		embed.add_field(name="!corona", value="To get global corona virus status", inline=True)
+		embed.add_field(name="!corona countryname", value="To get corona virus status in particular country", inline=True)
+		embed.add_field(name="!corona list", value="To get list", inline=True)
+		embed.add_field(name="!corona list n", value="To get list n. Here n is page number(1-4)", inline=True)
+		embed.set_footer(text="Type '!corona help' to know, how to see the other pages")
+		
+		await message.channel.send(embed=embed)
 	
 	#checks when someone says corona
 	if message.content.startswith("!corona"):
@@ -53,7 +56,15 @@ async def on_message(message):
 			data, place = getcases() 
 			confirmed, recovered, death = data["confirmed"]["value"], data["recovered"]["value"], data["deaths"]["value"]
 			active = confirmed - (recovered + death)
-			await message.channel.send(' **'+place.upper()+' CORONA CASES STATUS**\n**Active cases:** '+str(active)+'\n**Recovered:** '+str(recovered)+'\n**Death:** '+str(death)+'\n**Total Confirmed:** '+str(confirmed)+'')
+
+			embed=discord.Embed(title= place.capitalize()+" Corona Cases Status", color=discord.Color.blue())
+			embed.add_field(name="Active Cases", value=active, inline=True)
+			embed.add_field(name="Recovered", value=recovered, inline=True)
+			embed.add_field(name="Deaths", value=death, inline=True)
+			embed.add_field(name="Confirmed Cases", value= confirmed, inline=True)
+			embed.set_footer(text="Type '!corona help' to know, how to see the other pages")	
+			
+			await message.channel.send(embed = embed) 
 
 		country = message.content.split() 
 		if len(country) >= 2:
@@ -65,24 +76,48 @@ async def on_message(message):
 				if len(country) > 2: 
 					print(country)
 					if country[2] == "1":
-						await message.channel.send("List of countries available:\n```yaml\n"+pages[0]+"```")
+						embed=discord.Embed(title="List of countries available", color=discord.Color.blue())
+						embed.add_field(name="page 1 of 4", value=pages[0], inline=False)
+						embed.set_footer(text="Type '!corona help' to know, how to see the other pages")
+						
+						await message.channel.send(embed=embed)
 					if country[2] == "2":
-						await message.channel.send("List of countries available:\n```yaml\n"+pages[1]+"```")
+						embed=discord.Embed(title="List of countries available", color=discord.Color.blue())
+						embed.add_field(name="page 2 of 4", value=pages[1], inline=False)
+						embed.set_footer(text="Type '!corona help' to know, how to see the other pages")
+						
+						await message.channel.send(embed=embed)
 					if country[2] == "3":
-						await message.channel.send("List of countries available:\n```yaml\n"+pages[2]+"```")
+						embed=discord.Embed(title="List of countries available", color=discord.Color.blue())
+						embed.add_field(name="page 3 of 4", value=pages[2], inline=False)
+						embed.set_footer(text="Type '!corona help' to know, how to see the other pages")
+						
+						await message.channel.send(embed=embed)
 					if country[2] == "4":
-						await message.channel.send("List of countries available:\n```yaml\n"+pages[3]+"```")
+						embed=discord.Embed(title="List of countries available", color=discord.Color.blue())
+						embed.add_field(name="page 4 of 4", value=pages[3], inline=False)
+						embed.set_footer(text="Type '!corona help' to know, how to see the other pages")
+						
+						await message.channel.send(embed=embed)
 			
 				else:
-					await message.channel.send("List of countries available:\n```yaml\n"+pages[0]+"```Type `!corona help` to know, how to see the other pages")
+					embed=discord.Embed(title="List of countries available", color=discord.Color.blue())
+					embed.add_field(name="page 1 of 4", value=pages[0], inline=False)
+					embed.set_footer(text="Type '!corona help' to know, how to see the other pages")
+					
+					await message.channel.send(embed=embed)
 
 			elif  country[1] == "help":
-						await message.channel.send('''**use these following commands:**
-`!hello` or `!corona help` - Says hello and shows how to open help
-`!corona` - To get global corona virus status
-`!corona countryname` - To get corona virus status in particular country
-`!corona list` - To get list 1
-`!corona list n` - To get list n. Here n is page number(1-4)''')
+				embed=discord.Embed(title="Help for Corona Bot", color=discord.Color.blue())
+				embed.add_field(name="!corona help", value="Says hello and shows how to open help", inline=True)
+				embed.add_field(name="!corona", value="To get global corona virus status", inline=True)
+				embed.add_field(name="!corona countryname", value="To get corona virus status in particular country", inline=True)
+				embed.add_field(name="!corona list", value="To get list", inline=True)
+				embed.add_field(name="!corona list n", value="To get list n. Here n is page number(1-4)", inline=True)
+				embed.set_footer(text="Type '!corona help' to know, how to see the other pages")
+				
+				await message.channel.send(embed=embed)
+			
 			else:
 				Country = ""
 
@@ -95,7 +130,13 @@ async def on_message(message):
 					data, place = getcases(Country) 
 					confirmed, recovered, death = data["confirmed"]["value"], data["recovered"]["value"], data["deaths"]["value"]
 					active = confirmed - (recovered + death)
-					await message.channel.send(' **'+place.upper()+' CORONA CASES STATUS**\n**Active cases:** '+str(active)+'\n**Recovered:** '+str(recovered)+'\n**Death:** '+str(death)+'\n**Total Confirmed:** '+str(confirmed)+'') 
+					embed=discord.Embed(title= place.capitalize()+"Corona Cases Status", color=discord.Color.blue())
+					embed.add_field(name="Active Cases", value=active, inline=True)
+					embed.add_field(name="Recovered", value=recovered, inline=True)
+					embed.add_field(name="Deaths", value=death, inline=True)
+					embed.add_field(name="Confirmed Cases", value= confirmed, inline=True)
+					embed.set_footer(text="Type '!corona help' to know, how to see the other pages")
+					await message.channel.send(embed = embed) 
 				
 				#This will work once author gives incorrect value to !corona token
 				except KeyError:
